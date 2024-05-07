e0f402b130992d6fe3fa44a6c4a2a4c0be54f8e1
Mayuree Reunsati <mreunsat@noreply.01.gritlab.ax>
2024-05-07 20:26:19 +0000

    Update 'lookagain.sh'

--- lookagain.sh
+++ lookagain.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-# find . -name "*.sh" | tr '.sh' 000 | sed 's/0/ /g' | tr -d ' ' | sort -r
-find . -name "*.sh" | sed 's/.sh//' | sed 's/.*\///'| sort -r
+find . -name '*.sh' | sed 's/.sh//' | sed 's/.*\///'| sort -r
