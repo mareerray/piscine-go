5873eccc30a5eed31fcf98a41c6049aab8367838
mreunsat <mareerray@gmail.com>
2024-05-13 15:20:13 +0300

    YES

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,15 +4,16 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
+
+	if nb >= 21 {
+		return 0
+	}
+
 	var result int
 	result = 1
 
 	for i := 1; i <= nb; i++ {
 		result = result * i
-	}
-
-	if nb >= 21 {
-		return 0
 	}
 
 	return result
