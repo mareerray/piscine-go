bb7fbad373e6cfb2099d1a0f3f2df563da21f94b
Christopher Fremond <dev@01-edu.org>
2024-05-15 16:28:52 +0000

    Update 'lastrune.go'

--- lastrune.go
+++ lastrune.go
@@ -7,11 +7,3 @@ func LastRune(s string) rune {
 	}
 	return last[len(last)-1] // accesses the last element of the slice, and return it
 }
-
-/*Goal of the task:function that returns the last rune of a string.
-task1 : convert from a string to a slice of runes to ensure that
-each element in the slice represents one Unicode character.
-task2 : if statement checks if the length of the runes slice is zero.
-task3 : runes[len(runes)-1] accesses the last element of the slice,
-which is the last rune (character) of the input string s.
-*/
