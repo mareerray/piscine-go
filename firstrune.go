416cb197be5f2f3c9cea5dbed2c11ed88e8b73f2
mreunsat <mareerray@gmail.com>
2024-05-15 20:54:26 +0300

    "yeahhhh"

--- firstrune.go
+++ firstrune.go
@@ -1,12 +1,8 @@
 package piscine
 
-func FirstRune(s string) rune { // function FirstRune iterates over the string s using a range-based for loop.
-
-	for _, one := range s { // The loop returns the first rune (one) it encounters.
-
-		return one // The loop exits immediately after returning the first rune.
+func FirstRune(s string) rune {
+	for _, one := range s {
+		return one
 	}
-	return 0 // This ensures the function returns a valid rune
+	return 0
 }
-
-// Goal of the task: function that returns the first rune of a string.
