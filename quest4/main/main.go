a432007afd497f1a2a606e5392196008c7e0b36b
mreunsat <mareerray@gmail.com>
2024-05-16 13:58:07 +0300

    ascii chart

--- quest4/main/main.go
+++ quest4/main/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+
 	"piscine"
 )
 
 func main() {
-	fmt.Println(piscine.IsNumeric("010203"))
-	fmt.Println(piscine.IsNumeric("01,02,03"))
+	fmt.Println(piscine.IsPrintable("Hello"))
+	fmt.Println(piscine.IsPrintable("Hello\n"))
 }
+
+/*
+true
+false
+*/
