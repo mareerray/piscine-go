a397e9cf6f7503ddc8b0a675ec2588ac6fc095b1
mreunsat <mareerray@gmail.com>
2024-05-16 12:51:08 +0300

    char and slice once again

--- quest4/main/main.go
+++ quest4/main/main.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IsLower("hello"))
-	fmt.Println(piscine.IsLower("hello!"))
+	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
+	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
+	fmt.Println(piscine.IsAlpha("What's this 4?"))
+	fmt.Println(piscine.IsAlpha("Whatsthis4"))
 }
+
+/*
+false
+true
+false
+true
+*/
