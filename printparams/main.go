f6dee243654fd1e2ffb5c72ebd021e6b10d101f7
mreunsat <mareerray@gmail.com>
2024-05-16 21:38:12 +0300

    something

---
+++ printparams/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		for _, r := range arg {
+			z01.PrintRune(r)
+		}
+		z01.PrintRune('\n')
+	}
+}
