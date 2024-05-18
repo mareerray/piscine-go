88682acd8663fe6fc0aec87e6a2c80fcfa781f9f
mreunsat <mayuree.reunsati@gitlab.ax>
2024-05-18 20:47:42 +0300

    concatparams

--- quest4/main/main.go
+++ quest4/main/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-
 	"piscine"
 )
 
 func main() {
 	test := []string{"Hello", "how", "are", "you?"}
 	fmt.Println(piscine.ConcatParams(test))
+	// or we can do like this
+	// result := ConcatParams(arg)
+	// fmt.Println(result)
 }
