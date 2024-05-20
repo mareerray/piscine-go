d5de0576bab899363a8b0600b51676ea82815b29
mreunsat <mayuree.reunsati@gitlab.ax>
2024-05-20 21:24:25 +0300

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	argument := os.Args[1:]
+	if len(argument) == 0 {
+		fmt.Println("File name missing")
+		return
+	}
+	if len(argument) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	fileName := argument[0]
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Failed to read file", err)
+	}
+
+	// Convert the byte slice to a string and print it
+	fmt.Println(string(content))
+}
