42773134603c1c47aa41f8f7e4ad4e10cc011d9a
mreunsat <mayuree.reunsati@gitlab.ax>
2024-05-21 17:00:33 +0300

    IsNumeric function

--- any.go
+++ any.go
@@ -1,29 +1,12 @@
 package piscine
 
-var anyTrue bool //// Global variable to track if any string is numeric
-
 func Any(f func(string) bool, a []string) bool {
 	// passing item from the slice through the fucntion IsNumeric
+
 	for _, value := range a {
 		if f(value) {
-			anyTrue = true
-			break //// No need to check further elements since we found one that matches
+			return true
 		}
 	}
-	return anyTrue
+	return false
 }
-
-/*
-package piscine
-func IsNumeric(s string) bool {
- if len(s) == 0 {
- return false
- }
- for _, char := range s {
- if !(char >= 48 && char <= 57) {
- return false
- }
- }
- return true
-}
-*/
