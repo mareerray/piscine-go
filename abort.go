fce1c803912e44c9c7efe977bb3ae90b7c19c42f
mreunsat <mayuree.reunsati@gritlab.ax>
2024-05-23 02:04:04 +0300

    hackathon II

--- abort.go
+++ abort.go
@@ -13,15 +13,3 @@ func Abort(a, b, c, d, e int) int {
 	}
 	return (numbers[n/2])
 }
-
-// Starts the outer loop of the bubble sort algorithm.
-// The loop iterates over each element in the numbers slice except for the last one.
-// The -1 ensures that the last element is not compared with itself or with the last unsorted element.
-// Begins the inner loop, which iterates through the unsorted part of the numbers slice.
-// The expression n-i-1 calculates the number of remaining elements that need to be checked for sorting.
-// As the outer loop progresses, fewer elements remain unsorted, hence the decrement.
-// Checks if the current element (numbers[j]) is greater than the next element (numbers[j+1]).
-// If true, it swaps these two elements. This is the core operation of the bubble sort algorithm,
-// where larger elements "bubble up" towards the end of the slice.
-// The expression n/2 calculates the index of the middle element,
-// assuming n is the total number of elements in the slice.
