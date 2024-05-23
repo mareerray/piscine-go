216f77659ff4e49fb4d2c0e1803ba07fb6bf8d34
mreunsat <mayuree.reunsati@gritlab.ax>
2024-05-23 22:08:02 +0300

    practice

---
+++ practice/main.go
@@ -0,0 +1,39 @@
+/*package main
+
+import "github.com/01-edu/z01" */
+
+/*func main() {
+	for i := 'a'; i <= 'z'; i++ {
+		z01.PrintRune(rune(i))
+	}
+	z01.PrintRune('\n')
+}*/
+
+/*func main() {
+	for i := 'z'; i >= 'a'; i-- {
+		z01.PrintRune(rune(i))
+	}
+	z01.PrintRune('\n')
+}*/
+
+/*
+	func main() {
+		for i := '0'; i <= '9'; i++ {
+			z01.PrintRune(rune(i))
+		}
+		z01.PrintRune('\n')
+	}
+*/
+package main
+
+import "piscine"
+
+/*func main() {
+	piscine.IsNegative(20)
+	piscine.IsNegative(0)
+	piscine.IsNegative(-8)
+}*/
+
+func main() {
+	piscine.PrintComb()
+}
