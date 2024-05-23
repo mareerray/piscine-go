7c92c464fdee0782c5c02329d4b6630260958d85
mreunsat <mayuree.reunsati@gritlab.ax>
2024-05-23 15:11:35 +0300

    issorted

---
+++ issorted.go
@@ -0,0 +1,21 @@
+package piscine
+
+// Define the comparison function f
+
+func IsSorted(f func(a1, a2 int) int, tab []int) bool {
+	var sorted bool = true
+	for i := 0; i < len(tab)-1; i++ {
+		if f(tab[i], tab[i+1]) > 0 {
+			sorted = false
+		}
+	}
+	if !sorted {
+		sorted = true
+		for i := 0; i < len(tab)-1; i++ {
+			if f(tab[i], tab[i+1]) < 0 {
+				sorted = false
+			}
+		}
+	}
+	return sorted
+}
